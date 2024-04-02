26010fb483620eb2b6da5902ca8305a6a5d9bef9
Danxelo118 <danieldezemma@gmail.com>
2024-04-02 16:20:29 -0400

    3rd update

--- script/script.js
+++ script/script.js
@@ -2,3 +2,7 @@ document.querySelector('.scroll-cta').addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector('#scroll-target').scrollIntoView({ behavior: 'smooth' });
 });
+
+
+const ctaIcon = document.querySelector('.scroll-cta i');
+gsap.to(ctaIcon, { duration: 0.5, y: 5, repeat: -1, yoyo: true });
