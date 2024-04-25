386e8922df259c2127fb9b4bfb9fc1133f986645
Danxelo118 <danieldezemma@gmail.com>
2024-04-25 17:33:51 -0400

    update-11

--- script/script.js
+++ script/script.js
@@ -3,88 +3,115 @@ const ctaIcon = document.querySelector('.scroll-cta i');
 
 
 gsap.to("#cta-icon", {
-    y: 20, 
+    y: 20,
     duration: 1,
     repeat: -1,
-    yoyo: true, 
+    yoyo: true,
     ease: "power1.inOut"
-  });
-  
-  window.addEventListener('scroll', function() {
-    console.log('Scroll détecté !');
 });
+
+
 
 
 const body = document.querySelector('is-scrolling');
 
 let scrollTimer;
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     document.body.classList.add('is-scrolling');
 
 });
 
-window.addEventListener('scrollend', function() {
-    scrollTimer = setTimeout(function() {
+window.addEventListener('scrollend', function () {
+    scrollTimer = setTimeout(function () {
         document.body.classList.remove('is-scrolling');
     }, 100);
-   
+
 })
 
 gsap.to("#sprite-sheet-bird", {
-    backgroundPosition: "-100% 0",
-    duration: 5, 
-    repeat: -1, 
-    ease: "steps(15)", 
-    x: "100vw", 
-  });
-  
+    x: "-30vw",
+    x: "+=100vw",
+    duration: 50,
+    repeat: -1,
+})
 
-
+gsap.to("#sprite-sheet-whale", {
+    x: "-30vw",
+    x: "+=100vw",
+    duration: 50,
+    repeat: -1,
+})
 
 gsap.to("#desert-ball", {
     x: "100vw",
     duration: 3,
-    ease: "linear", 
-    rotation: 360, 
-    repeat: -1 
-  });
-
-  gsap.to("#planete-1", {
-    duration: 5,
-    ease: "linear", 
-    rotation: 90, 
-    repeat: -1 
-  });
-
-
-  
-  let planets = gsap.timeline({ repeat: -1});
-
-
-  planets.to("#planete-2", {
-    duration: 5,
-    ease: "linear", 
+    ease: "linear",
     rotation: 360,
-  });
-  
-  planets.to("#planete-3", {
-    duration: 5,
-    ease: "linear", 
-    rotation: 360, 
-}, "-=5");
+    repeat: -1,
+});
+
+gsap.to("#planete-1", {
+    duration: 2,
+    ease: "linear",
+    rotation: 360,
+    repeat: -1
+});
+
+
+
+let planets = gsap.timeline({ repeat: -1 });
+
+
+planets.to("#planete-2", {
+    duration: 3,
+    ease: "power1.inOut",
+    y: "100vh",
+    rotation: 360,
+});
+
+planets.to("#planete-3", {
+    duration: 3,
+    ease: "power1.inOut",
+    y: "100vh",
+    rotation: 360,
+}, "-=0.5");
+
+planets.to("#planete-2", {
+    duration: 3,
+    ease: "linear",
+    y: 0,
+    rotation: 180,
+});
+
+
+planets.to("#planete-3", {
+    duration: 3,
+    ease: "linear",
+    y: 0,
+    rotation: 180,
+}, "-=0.5");
+
 
 gsap.to("#spaceship", {
     x: "-100vw",
     duration: 2,
-    ease: "linear",  
-    repeat: -1 
+    ease: "power4.inOut",
+    repeat: -1
 });
 
 gsap.to("#spaceship", {
-    scale: 0.5, 
+    scale: 0.5,
     duration: 1,
-    ease: "linear",
+    ease: "power4.inOut",
     repeat: -1,
-    yoyo: true, 
-  });
+    yoyo: true,
+});
+
+gsap.to("#bubble", {
+    opacity: 0,
+    repeat: -1,
+    yoyo: true,
+    duration: 1,
+    ease: "power4.inOut",
+})
